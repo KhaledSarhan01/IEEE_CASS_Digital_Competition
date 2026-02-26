7fc005462d5d42b7327040dbf1412220286cc972
KhaledSarhan01 <khaledsarhaan@gmail.com>
2026-02-26 15:58:40 +0200

    RAM Design is updated

--- design/rtl/RAM.sv
+++ design/rtl/RAM.sv
@@ -1,21 +1,42 @@
 module RAM #(
-    parameter int MEM_SIZE   = 1024,
-    parameter int DATA_WIDTH = 8 
+    parameter int MEM_SIZE = 512,
+    parameter int DATA_WIDTH = 16,
+    parameter FILE_NAME = "random_bytes.mem" // Removed "string" keyword here
 ) (
-    input  logic                        clk,
-    input  logic [$clog2(MEM_SIZE)-1:0] read_addr,
+    // Input Write Port
+    input  logic                        in_clk,
     input  logic [$clog2(MEM_SIZE)-1:0] write_addr,
     input  logic [DATA_WIDTH-1:0]       data_in,
     input  logic                        write_enable,
-    output logic [DATA_WIDTH-1:0]       data_out
+    input  logic 				    	in_done,
+    // Output Read Port
+    input  logic                        out_clk,
+    input  logic [$clog2(MEM_SIZE)-1:0] read_addr,
+    output logic [DATA_WIDTH-1:0]       data_out,
+    output logic 				    	out_done
 );
     
-    (* ramstyle = "M10K" *) logic [DATA_WIDTH-1:0] ram_mem [0:MEM_SIZE-1];
+    (* ramstyle = "M10K" *) logic [DATA_WIDTH-1:0] M10k_mem [0:MEM_SIZE-1];
 
-    always_ff @(posedge clk) begin
+    initial begin
+        // Quartus prefers the parameter to be passed without strict string typing
+        // or defined as a literal.
+        $readmemh(FILE_NAME, M10k_mem);
+    end 
+
+    always_ff @(posedge in_clk) begin
         if (write_enable) begin 
-            ram_mem[write_addr] <= data_in;
+            M10k_mem[write_addr] <= data_in;
         end
-        data_out <= ram_mem[read_addr];
-    end    
+    end
+	 always_ff @(posedge out_clk) begin
+			data_out <= M10k_mem[read_addr];
+	 end 
+	 
+	 logic [1:0] reg_2ff_sync;
+	 always @(posedge out_clk)begin
+		reg_2ff_sync[0] <= in_done;
+		reg_2ff_sync[1] <= reg_2ff_sync[0];
+	 end 
+	 assign out_done = reg_2ff_sync[1];  
 endmodule
